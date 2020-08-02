7af332554d1af9b3c0b317c8159481294ab9de41
Fabrice LE FESSANT <fabrice.le_fessan@ocamlpro.com>
2020-08-02 20:19:38 +0200

    Added a FAQ

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'z-2020-08-01-use-patches-only (fdd7e9d) (2020/08/02 13:20)',
+    VERSION: 'z-2020-08-01-use-patches-only (e9c9199) (2020/08/02 20:18)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
