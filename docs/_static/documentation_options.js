920545c7116640dba503dc5a3352d782d9370d93
Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
2020-08-31 06:56:23 +0200

    fix workflows

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'z-2020-08-18-opam-bin-new (073b7d0) (2020/08/29 11:05)',
+    VERSION: 'z-2020-08-29-use-drom (d97e44e) (2020/08/31 06:55)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
