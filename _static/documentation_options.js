8e0ada604a4dab69e50f352f7d1dd71b2ec3ad91
lefessan <fabrice.le_fessant@ocamlpro.com>
2022-12-27 17:38:13 +0000

    Deploying to gh-pages from  @ f0476a3f36f53232731c5f6082eb468cf3388668 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (f0476a3) (2022/12/27 17:35)',
+    VERSION: 'master (f0476a3) (2022/12/27 17:38)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
